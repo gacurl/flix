4a368b63d55c86c70fff5aa8b7e0c403dd1bc5d9
gacurl <greg.a.curl@gmail.com>
2023-06-04 15:11:01 -0400

    add view helpers

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,19 @@
 module MoviesHelper
+
+    def total_gross(movie)
+      if movie.flop?
+        "Flop!"
+      else
+        number_to_currency(movie.total_gross, precision: 0)
+      end
+    end
+
+    def desc(movie)
+      truncate(movie.description, length: 40, separator: ' ')
+    end
+
+    def year_of(movie)
+      movie.released_on.year
+    end
+  
 end
