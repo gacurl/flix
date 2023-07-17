a285f5f58555202f47cc79c919d7e74b9d00300a
gacurl <greg.a.curl@gmail.com>
2023-07-17 16:34:24 -0400

    add admin to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,8 @@
 class UsersController < ApplicationController
 
+  before_action :require_signin, except: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :update, :destroy]
+
   def index
     @users = User.all
   end
@@ -15,6 +18,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id] = @user.id
       redirect_to @user, notice: "User successfully created!"
     else
       render :new, status: :unprocessable_entity
@@ -22,11 +26,9 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
     if @user.update(user_params)
       redirect_to @user, notice: "Account successfully updated!"
     else
@@ -35,8 +37,8 @@ class UsersController < ApplicationController
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
+    session[:user_id] = nil
     redirect_to users_url, status: :see_other,
       alert: "Account successfully deleted"
   end
@@ -46,6 +48,11 @@ private
   def user_params
     params.require(:user).
       permit(:username, :name, :email, :password, :password_confirmation)
+  end
+
+  def require_correct_user
+    @user = User.find(params[:id])
+    redirect_to root_url, status: :see_other unless current_user(@user)
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     resources :reviews
   end
   
-  resource :session, only: [ :new, :create, :destroy ]
+  resource :session, only: [:new, :create, :destroy]
   resources :users
 
   get "signup" => "users#new"
